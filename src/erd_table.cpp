13559f93b212c0b45f65c04085260e98d69f3ee4
jankup <jankup@ab01d90b-4ff7-4e84-82d5-2368d4a8d7fa>
2011-01-03 15:08:24 +0000

    git-svn-id: https://wxdbexplorer.svn.sourceforge.net/svnroot/wxdbexplorer/trunk@9 ab01d90b-4ff7-4e84-82d5-2368d4a8d7fa

--- src/erd_table.cpp
+++ src/erd_table.cpp
@@ -26,6 +26,9 @@ void ErdTable::Initialize()
 {
 	SetFill(wxBrush(wxColour(254, 253, 211)));
 	m_pLabel = new wxSFTextShape();
+	
+	m_pGrid = new wxSFFlexGridShape();
+	
 	if (m_pLabel){
 		m_pLabel->SetVAlign(wxSFShapeBase::valignTOP);
         m_pLabel->SetHAlign(wxSFShapeBase::halignCENTER);
@@ -36,6 +39,24 @@ void ErdTable::Initialize()
 		m_pLabel->SetStyle( sfsHOVERING | sfsALWAYS_INSIDE | sfsPROCESS_DEL | sfsEMIT_EVENTS );
 
         SF_ADD_COMPONENT( m_pLabel, wxT("title") );		
+		
+		
+		// set grid
+		m_pGrid->SetRelativePosition( 0, 20 );
+		m_pGrid->SetStyle( sfsALWAYS_INSIDE | sfsPROCESS_DEL );
+		m_pGrid->SetDimensions( 1, 1 );
+		
+		m_pGrid->SetFill( *wxTRANSPARENT_BRUSH );
+		m_pGrid->SetBorder( *wxTRANSPARENT_PEN );
+		
+		m_pGrid->SetHAlign( wxSFShapeBase::halignLEFT );
+		m_pGrid->SetHBorder( 0 );
+		m_pGrid->SetVBorder( 5 );
+		m_pGrid->SetCellSpace( 10 );
+		
+		m_pGrid->AcceptChild( wxT("All") );
+		SF_ADD_COMPONENT( m_pGrid, wxT("main_grid") );
+		
 		}
 }
 
